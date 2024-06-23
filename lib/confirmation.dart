18f085526193d2391242348b8c72f6789ce810a4
rijwan-uddin <rijwan35-653@diu.edu.bd>
2024-06-24 02:20:08 +0600

    Payment method added

---
+++ lib/confirmation.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class confirmation extends StatefulWidget {
+  const confirmation({super.key});
+
+  @override
+  State<confirmation> createState() => _confirmationState();
+}
+
+class _confirmationState extends State<confirmation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+
+    );
+  }
+}
